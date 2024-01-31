40b52261f953248a952a2aa46764e6cdd3fc7e03
juuhadg <juku.jj@gmail.com>
2024-01-31 16:50:24 -0300

    augments and string matchin done

--- selecionarAprimoramento.py
+++ selecionarAprimoramento.py
@@ -4,7 +4,7 @@ from lerImage import lerImagem
 import time
 from unidecode import unidecode
 from obterDadosAugments import obterAugments
-from tratarString import tratarString
+from tratarString import stringMatch
 import win32api
 import win32con
 
@@ -46,16 +46,38 @@ def selecionarMelhorAprimoramento(augment_data):
     print(aprimoramento2)
     print(aprimoramento3)
 
-    aprimoramento1 =  next((augment for augment in augment_data if tratarString(augment.get('nome')) == tratarString(aprimoramento1)), None)
-    aprimoramento2 = next((augment for augment in augment_data if tratarString(augment.get('nome')) == tratarString(aprimoramento2)), None)
-    aprimoramento3 = next((augment for augment in augment_data if tratarString(augment.get('nome')) == tratarString(aprimoramento3)), None)
+   
+    aprimoramento1 = next((augment for augment in augment_data if stringMatch(str(aprimoramento1), augment.get('nome'), minimumMatchRate=0.6) == True), None)
 
-    aprimoramento1['index'] = 1
-    aprimoramento2['index'] = 2
-    aprimoramento3['index'] = 3
-    
+    aprimoramento2 = next((augment for augment in augment_data if stringMatch(str(aprimoramento2), augment.get('nome'), minimumMatchRate=0.6) == True), None)
 
-    opcoes = [aprimoramento1,aprimoramento2,aprimoramento3]
+    aprimoramento3 = next((augment for augment in augment_data if stringMatch(str(aprimoramento3), augment.get('nome'), minimumMatchRate=0.6) == True), None)
+
+
+    print('\n')
+    print(aprimoramento1)
+    print(aprimoramento2)
+    print(aprimoramento3)
+
+    #aprimoramento2 = next((augment for augment in augment_data if tratarString(augment.get('nome')) == tratarString(aprimoramento2)), None)
+
+    #aprimoramento3 = next((augment for augment in augment_data if tratarString(augment.get('nome')) == tratarString(aprimoramento3)), None)
+    opcoes = []
+    if aprimoramento1 != None:
+        aprimoramento1['index'] = 1
+        opcoes.append(aprimoramento1)
+
+    if aprimoramento2 != None:
+        aprimoramento2['index'] = 2
+        opcoes.append(aprimoramento2)
+
+    if aprimoramento3 != None:
+        aprimoramento3['index'] = 3
+        opcoes.append(aprimoramento3)
+
+       
+
+   
     vencedor = None
     melhorPlacement = 8
 
@@ -63,16 +85,22 @@ def selecionarMelhorAprimoramento(augment_data):
         if opcao.get('placement') < melhorPlacement:
             melhorPlacement = opcao.get('placement')
             vencedor = opcao
-    
+        
+        
+        
     selecionado = pyautogui.locateOnScreen(f'./images/temp/augment{vencedor["index"]}.png')
+
+    print(f'selecionado : {selecionado}')
+    
     if selecionado is not None:
         win32api.SetCursorPos((selecionado.left,selecionado.top))
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
         time.sleep(0.1)  
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
         print(f' aprimoramento selecionado : {vencedor["nome"]} , com colocação média de {vencedor["placement"]} , clicado em {selecionado.left} , {selecionado.top}')
+   
 
 
-
+selecionarMelhorAprimoramento(augments)
 
 
